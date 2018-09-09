f29405baa93f55c42fcb451021fac79193a3c5bc
llTONYll <tinnaput_2535@hotmail.com>
2018-09-09 15:41:19 +0700

    byte to bitmap

--- OMR-ANS-SHEET/OMR/OMRv1.cs
+++ OMR-ANS-SHEET/OMR/OMRv1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace OMR
 {
@@ -10,6 +12,7 @@ namespace OMR
     {
         public bool GetPositionPoint(byte[] data, int width, int height, int pitch, out List<PointProperty> points, out int pointSize)
         {
+            Bitmap bitmap = new Bitmap(width, height, pitch, System.Drawing.Imaging.PixelFormat.Format24bppRgb, Marshal.UnsafeAddrOfPinnedArrayElement(data, 0));
             throw new NotImplementedException();
         }
     }
