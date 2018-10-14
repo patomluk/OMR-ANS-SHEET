82aac076606ed21c75cf1b33ae1aefd3d3c1a339
llTONYll <tinnaput_2535@hotmail.com>
2018-10-14 16:46:03 +0700

    add ironpy

---
+++ OMR-ANS-SHEET/OMR/OMRv2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMR
+{
+    public class OMRv2 : OMR.IOMR
+    {
+        public (List<PointProperty> pointsList, List<int> rowSize) GetPositionPoint(Bitmap bitmap, bool getCheck = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
